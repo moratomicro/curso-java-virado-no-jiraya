c7d0a4d06a51e741234aff3969a8c9384176156a
moratomicro <moratomicro@yahoo.com.br>
2021-11-30 21:36:41 -0300

    Criação e compilação da primeira classe java

---
+++ src/br/com/moratomicro/maratonajava/introducao/Aula01OlaDevDojo.java
@@ -0,0 +1,17 @@
+package br.com.moratomicro.maratonajava.introducao;
+
+/**
+ * Isso é um comentário <b>javadoc</b>
+ */
+
+public class Aula01OlaDevDojo {
+    //Isso é um comentário de uma linha
+    /*
+        Isso é um comentário
+        de multiplas
+        linhas.
+     */
+    public static void main(String[] args) {
+        System.out.println("Ka-ME-HA-ME-HAAAAA!");
+    }
+}
