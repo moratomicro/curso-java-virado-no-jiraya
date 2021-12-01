e09f3f7c5c6682711110cfd65aa1299dbdbc59ec
moratomicro <moratomicro@yahoo.com.br>
2021-11-30 21:51:46 -0300

    Tipos Primitivos pt 01 - convenções de variáveis

--- src/br/com/moratomicro/maratonajava/introducao/Aula02TiposPrimitivos.java
+++ src/br/com/moratomicro/maratonajava/introducao/Aula02TiposPrimitivos.java
@@ -4,6 +4,6 @@ public class Aula02TiposPrimitivos {
     public static void main(String[] args) {
         // int, double, float, char, byte, short, long, boolean
         int idade = 10;
-        System.out.println(idade);
+        System.out.println("A idade é "+idade+" anos.");
     }
 }
