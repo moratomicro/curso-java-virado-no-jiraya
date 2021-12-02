deef45c046fc7463461e755746911f7ce89bd890
moratomicro <moratomicro@yahoo.com.br>
2021-12-01 21:56:34 -0300

    Operadores - Parte 01 - Aritiméticos

---
+++ src/br/com/moratomicro/maratonajava/introducao/Aula04Operadores.java
@@ -0,0 +1,11 @@
+package br.com.moratomicro.maratonajava.introducao;
+
+public class Aula04Operadores {
+    public static void main(String[] args) {
+        // + - / *
+        int numero01 = 10;
+        int numero02 = 20;
+        int resultado = numero02 / numero01;
+        System.out.println("Valor = "+resultado);
+    }
+}
