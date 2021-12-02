6a193ea94859258a88d13ee198b6e41b60632c5e
moratomicro <moratomicro@yahoo.com.br>
2021-12-02 09:37:50 -0300

    Operadores - Parte 02 - Relacionais

--- src/br/com/moratomicro/maratonajava/introducao/Aula04Operadores.java
+++ src/br/com/moratomicro/maratonajava/introducao/Aula04Operadores.java
@@ -6,6 +6,25 @@ public class Aula04Operadores {
         int numero01 = 10;
         int numero02 = 20;
         int resultado = numero02 / numero01;
+        System.out.println("_______________________Aritiméticos__________________________");
         System.out.println("Valor = "+resultado);
+
+        // %
+        int resto = 20 % 2;
+        System.out.println("_______________________Relacionais__________________________");
+        System.out.println(resto);
+
+        // <> <= >= == !=
+        boolean isDezMaiorQueVinte = 10 > 20;
+        boolean isDezMenorQueVinte = 10 < 20;
+        boolean isDezIgualVinte = 10 == 20;
+        boolean isDezIgualDez = 10 == 10;
+        boolean isDezDiferenteDez = 10 != 10;
+        System.out.println("_______________________Lógicos__________________________");
+        System.out.println("isDezMaiorQueVinte " +isDezMaiorQueVinte);
+        System.out.println("isDezMenorQueVinte " +isDezMenorQueVinte);
+        System.out.println("isDezIgualVinte " +isDezIgualVinte);
+        System.out.println("isDezIgualDez " +isDezIgualDez);
+        System.out.println("isDezDiferenteDez " +isDezIgualDez);
     }
 }
