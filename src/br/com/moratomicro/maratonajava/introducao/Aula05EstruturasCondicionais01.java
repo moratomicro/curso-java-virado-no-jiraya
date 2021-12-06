a9c7d503acba58efdff97ad2cc645893959353ac
moratomicro <moratomicro@yahoo.com.br>
2021-12-06 12:56:04 -0300

    Estruturas Condicionais - Parte 0 - ELSE IF

--- src/br/com/moratomicro/maratonajava/introducao/Aula05EstruturasCondicionais01.java
+++ src/br/com/moratomicro/maratonajava/introducao/Aula05EstruturasCondicionais01.java
@@ -1,17 +1,24 @@
 package br.com.moratomicro.maratonajava.introducao;
 
-public class Aula05EstruturasCondicionais {
+public class Aula05EstruturasCondicionais01 {
     public static void main(String[] args) {
-        int idade = 10;
+        int idade = 15;
         boolean isAutorizadoComprarBebida = idade >=18;
+        // !
+
         if (isAutorizadoComprarBebida) {
-            System.out.println("Pode comprar");
+            System.out.println("Autorizado a comprar bebida alcólica");
+        } else{
+            System.out.println("Não Autorizado a compra bebida alcólica");
         }
 
         if(!isAutorizadoComprarBebida){
             System.out.println("Não Autorizado a compra bebida alcólica");
         }
-
+        boolean c = false;
+        if(c == true){
+            System.out.println("Dentro de algo que nunca deve ser feito");
+        }
         System.out.println("Não pode comprar");
 
     }
---
+++ src/br/com/moratomicro/maratonajava/introducao/Aula05EstruturasCondicionais02.java
@@ -0,0 +1,19 @@
+package br.com.moratomicro.maratonajava.introducao;
+
+public class Aula05EstruturasCondicionais02 {
+    public static void main(String[] args) {
+        // idade < 15 categoria infantil
+        // idade >= 15 && idade < 18 ategoria junvenil
+        // idade >=18 categoria adulto
+        int idade = 17;
+        String categoria;
+        if(idade < 15){
+            categoria = "Categoria Infantil";
+        }else if(idade >=15 && idade < 18){
+            categoria = "Categoria Juvenil";
+        }else {
+            categoria = "Categoria Adulto";
+        }
+        System.out.println(categoria);
+    }
+}
