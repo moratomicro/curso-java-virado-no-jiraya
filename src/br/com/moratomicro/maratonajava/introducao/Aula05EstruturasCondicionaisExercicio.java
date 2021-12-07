55b241370a8a673253c4bcf482f0dee6bc527f5d
moratomicro <moratomicro@yahoo.com.br>
2021-12-07 14:35:48 -0300

    Estrutuas Condicionais - Pate 05 - Exercício resolvido

--- src/br/com/moratomicro/maratonajava/introducao/Aula05EstruturasCondicionaisExercicio.java
+++ src/br/com/moratomicro/maratonajava/introducao/Aula05EstruturasCondicionaisExercicio.java
@@ -2,18 +2,19 @@ package br.com.moratomicro.maratonajava.introducao;
 
 public class Aula05EstruturasCondicionaisExercicio {
     public static void main(String[] args) {
-        double salarioAnualBruto = 70000;
-        double salarioAnualLiquido;
+        double salarioAnual = 25000;
+        double primeiraFaixa = (salarioAnual * 9.70) / 100;
+        double segundaFaixa  = (salarioAnual * 37.35) / 100;
+        double terceiraFaixa = (salarioAnual * 49.50) / 100;
+        double valorDoImposto;
 
-        if(salarioAnualBruto > 0 && salarioAnualBruto <= 20711){
-            salarioAnualLiquido = salarioAnualBruto + ((salarioAnualBruto * 36.65)/100);
-            System.out.println("Salário anual líquido = R$ " + salarioAnualLiquido);
-        }else if(salarioAnualBruto > 20711 && salarioAnualBruto <= 68507){
-            salarioAnualLiquido = salarioAnualBruto + ((salarioAnualBruto * 38.10)/100);
-            System.out.println("Salário anual líquido = R$ " + salarioAnualLiquido);
-        }else if(salarioAnualBruto > 68507){
-            salarioAnualLiquido = salarioAnualBruto + ((salarioAnualBruto * 51.75)/100);
-            System.out.println("Salário anual líquido = R$ " + salarioAnualLiquido);
+        if(salarioAnual > 0 && salarioAnual <= 20711){
+            valorDoImposto =  primeiraFaixa;
+        }else if(salarioAnual > 20711 && salarioAnual <= 68507){
+            valorDoImposto = segundaFaixa;
+        }else{
+            valorDoImposto = terceiraFaixa;
         }
+        System.out.println("Salário anual = R$ " +valorDoImposto);
     }
 }
