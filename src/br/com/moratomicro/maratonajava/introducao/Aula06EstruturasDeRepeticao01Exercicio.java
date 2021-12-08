6d483571d71a3140dd66137c609b88510871cac3
moratomicro <moratomicro@yahoo.com.br>
2021-12-08 20:44:30 -0300

    Estrutura de Repetição - Parte 01 - Laços de Repetição - Exercício For

---
+++ src/br/com/moratomicro/maratonajava/introducao/Aula06EstruturasDeRepeticao01Exercicio.java
@@ -0,0 +1,17 @@
+package br.com.moratomicro.maratonajava.introducao;
+
+public class Aula06EstruturasDeRepeticao01Exercicio {
+    public static void main(String[] args) {
+        // Imprima todos os números pares de 0 até 1000000
+
+        /*for (int i = 0; i <= 1000000 ; i+=2) {
+            System.out.println(i);
+        }*/
+
+        for (int i = 0; i <= 1000000 ; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i);
+            }
+        }
+    }
+}
