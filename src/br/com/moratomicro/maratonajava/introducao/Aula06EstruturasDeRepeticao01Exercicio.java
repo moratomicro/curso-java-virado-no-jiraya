b1904d44106dc9538e440a5908beb0f048941a19
moratomicro <moratomicro@yahoo.com.br>
2021-12-09 11:02:59 -0300

    Estrutura de Repetição - Parte 03 - Break e Parte 04 - Exercício Break

--- src/br/com/moratomicro/maratonajava/introducao/Aula06EstruturasDeRepeticao01Exercicio.java
+++ src/br/com/moratomicro/maratonajava/introducao/Aula06EstruturasDeRepeticao01Exercicio.java
@@ -8,7 +8,7 @@ public class Aula06EstruturasDeRepeticao01Exercicio {
             System.out.println(i);
         }*/
 
-        for (int i = 0; i <= 1000000 ; i++) {
+        for (int i = 0; i <= 100 ; i++) {
             if (i % 2 == 0) {
                 System.out.println(i);
             }
