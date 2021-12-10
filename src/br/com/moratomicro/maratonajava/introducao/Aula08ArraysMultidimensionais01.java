85d861cbfebf0841ceae798881201c6f4dd5b03f
moratomicro <moratomicro@yahoo.com.br>
2021-12-10 15:42:02 -0300

    Arrays Multidimensionais - Parte 01

---
+++ src/br/com/moratomicro/maratonajava/introducao/Aula08ArraysMultidimensionais01.java
@@ -0,0 +1,27 @@
+package br.com.moratomicro.maratonajava.introducao;
+
+public class Aula08ArraysMultidimensionais01 {
+    public static void main(String[] args) {
+        // 1,2,3,4,5 => Meses
+        // 31,28,31,30 => dias dos Meses
+        // Queremos linkar os dias nos meses
+        int[][] dias = new int[3][3];
+        dias[0][0] = 31;
+        dias[0][1] = 28;
+        dias[0][2] = 31;
+
+        dias[1][0] = 30;
+        dias[1][1] = 31;
+        dias[1][2] = 30;
+
+        dias[2][0] = 30;
+        dias[2][1] = 30;
+        dias[2][2] = 31;
+
+        for (int[] dia : dias) {
+            for (int j = 0; j < dias[0].length; j++) {
+                System.out.println(dia[j]);
+            }
+        }
+    }
+}
