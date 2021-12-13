0c10b837f4605af853fb32022575ab10cb439290
moratomicro <moratomicro@yahoo.com.br>
2021-12-13 19:25:13 -0300

    Orientação a Objetos - Métodos - Parte 01

---
+++ src/br/com/moratomicro/maratonajava/javacore/BintroducaoMetodos/test/CalculadoraTest01.java
@@ -0,0 +1,13 @@
+package br.com.moratomicro.maratonajava.javacore.BintroducaoMetodos.test;
+
+import br.com.moratomicro.maratonajava.javacore.BintroducaoMetodos.dominio.Calculadora;
+
+public class CalculadoraTest01 {
+    public static void main(String[] args) {
+        Calculadora calculadora = new Calculadora();
+        calculadora.somaDoisNumeros();
+        System.out.println("Finalizando CalculadoraTest01 - somaDoisNumeros");
+        calculadora.subtraiDoisNumeros();
+        System.out.println("Finalizando CalculadoraTest01 - subtraiDoisNumeros");
+    }
+}
