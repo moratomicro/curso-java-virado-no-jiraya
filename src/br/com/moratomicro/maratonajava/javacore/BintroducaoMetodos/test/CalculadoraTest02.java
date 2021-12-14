7df079eecbfe09258f7c97114f8473f0bcff2f49
moratomicro <moratomicro@yahoo.com.br>
2021-12-14 09:21:53 -0300

    Orientação a Objetos - Métodos -  Parte 02 - Parãmetros

---
+++ src/br/com/moratomicro/maratonajava/javacore/BintroducaoMetodos/test/CalculadoraTest02.java
@@ -0,0 +1,10 @@
+package br.com.moratomicro.maratonajava.javacore.BintroducaoMetodos.test;
+
+import br.com.moratomicro.maratonajava.javacore.BintroducaoMetodos.dominio.Calculadora;
+
+public class CalculadoraTest02 {
+    public static void main(String[] args) {
+        Calculadora calculadora = new Calculadora();
+        calculadora.multiplicaDoisNumeros(10, 20.5F);
+    }
+}
