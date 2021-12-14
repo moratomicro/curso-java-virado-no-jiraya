2e925ef5a36107aea3c8a8b61725ec3c0062c007
moratomicro <moratomicro@yahoo.com.br>
2021-12-14 09:36:58 -0300

    Orientação a Objetos - Métodos -  Parte 03 - Retorno - Parte 01

---
+++ src/br/com/moratomicro/maratonajava/javacore/BintroducaoMetodos/test/CalculadoraTest03.java
@@ -0,0 +1,11 @@
+package br.com.moratomicro.maratonajava.javacore.BintroducaoMetodos.test;
+
+import br.com.moratomicro.maratonajava.javacore.BintroducaoMetodos.dominio.Calculadora;
+
+public class CalculadoraTest03 {
+    public static void main(String[] args) {
+        Calculadora calculadora = new Calculadora();
+        double result = calculadora.divideDoisNumeros(20,0);
+        System.out.println(result);
+    }
+}
