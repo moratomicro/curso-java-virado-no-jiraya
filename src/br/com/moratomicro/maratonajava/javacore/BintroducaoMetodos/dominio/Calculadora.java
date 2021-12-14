e33a984a9d43c7bf37bb424d1ef91dd111ab7fbb
moratomicro <moratomicro@yahoo.com.br>
2021-12-14 09:52:23 -0300

    Orientação a Objetos - Métodos -  Parte 04 - Retorno - Parte 02

--- src/br/com/moratomicro/maratonajava/javacore/BintroducaoMetodos/dominio/Calculadora.java
+++ src/br/com/moratomicro/maratonajava/javacore/BintroducaoMetodos/dominio/Calculadora.java
@@ -16,8 +16,26 @@ public class Calculadora {
 
     public double divideDoisNumeros(double num1, double num2) {
         if(num2 == 0){
+            System.out.println("Não existe divisão por zero!");
             return 0;
         }
         return num1/num2;
+    }
+
+    public double divideDoisNumeros02(double num1, double num2) {
+        if(num2 != 0){
+            return num1 / num2;
+        }
+        System.out.println("Não existe divisão por zero!");
+        return 0;
+    }
+
+    public void imprimeDivisaoDeDoisNumeros(double num1, double num2) {
+        if(num2 == 0) {
+            System.out.println("Não existe divisão por zero!");
+            return;
+        }
+        System.out.println(num1/num2);
+
     }
 }
--- src/br/com/moratomicro/maratonajava/javacore/BintroducaoMetodos/test/CalculadoraTest03.java
+++ src/br/com/moratomicro/maratonajava/javacore/BintroducaoMetodos/test/CalculadoraTest03.java
@@ -7,5 +7,9 @@ public class CalculadoraTest03 {
         Calculadora calculadora = new Calculadora();
         double result = calculadora.divideDoisNumeros(20,0);
         System.out.println(result);
+        System.out.println("___________________________");
+        System.out.println(calculadora.divideDoisNumeros02(30,0));
+        System.out.println("____________________________");
+        calculadora.imprimeDivisaoDeDoisNumeros(86,0);
     }
 }
