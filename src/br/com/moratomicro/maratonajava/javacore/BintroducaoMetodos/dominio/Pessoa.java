0fd545ab93446ea102c19f21880adf4ae5396c03
moratomicro <moratomicro@yahoo.com.br>
2021-12-20 13:51:22 -0300

    Orientação a Objetos - Modificador de acesso private, get e set - Parte 02

--- src/br/com/moratomicro/maratonajava/javacore/BintroducaoMetodos/dominio/Pessoa.java
+++ src/br/com/moratomicro/maratonajava/javacore/BintroducaoMetodos/dominio/Pessoa.java
@@ -4,14 +4,12 @@ public class Pessoa {
     private String nome;
     private int idade;
 
-    public String getNome() {
-        return nome;
-    }
+    public String getNome() { return nome; }
+    public void setNome(String nome) { this.nome = nome; }
 
-    public void setNome(String nome) {
-        this.nome = nome;
+    public int getIdade() {
+        return idade;
     }
-
     public void setIdade(int idade) {
         if(idade < 0){
             System.out.println("Idade inválida!");
@@ -20,12 +18,8 @@ public class Pessoa {
         this.idade = idade;
     }
 
-    public int getIdade() {
-        return idade;
-    }
-
     public void imprime() {
-        System.out.println(nome);
-        System.out.println(idade);
+        System.out.println(this.nome);
+        System.out.println(this.idade);
     }
 }
--- src/br/com/moratomicro/maratonajava/javacore/BintroducaoMetodos/test/PessoaTest01.java
+++ src/br/com/moratomicro/maratonajava/javacore/BintroducaoMetodos/test/PessoaTest01.java
@@ -5,11 +5,14 @@ import br.com.moratomicro.maratonajava.javacore.BintroducaoMetodos.dominio.Pesso
 public class PessoaTest01 {
     public static void main(String[] args) {
         Pessoa pessoa = new Pessoa();
-        pessoa.setNome("Jiraya");
-        pessoa.setIdade(-1);
+        /*pessoa.nome = "Jiraya";
+        pessoa.idade = 70;*/
 
-//        pessoa.imprime();
-        System.out.println(pessoa.getNome());
-        System.out.println(pessoa.getIdade());
+        pessoa.setNome("Jiraya");
+        pessoa.setIdade(70);
+
+        pessoa.imprime();
+        /*System.out.println(pessoa.getNome());
+        System.out.println(pessoa.getIdade());*/
     }
 }
