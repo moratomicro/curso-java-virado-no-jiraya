a702dbef6a8bcf50ce0579e9890cce9e650fc91d
moratomicro <moratomicro@yahoo.com.br>
2021-12-21 11:27:49 -0300

    Orientação a Objetos - Sobrecarga de Métodos

---
+++ src/br/com/moratomicro/maratonajava/javacore/Csobrecargametodos/test/AnimeTest01.java
@@ -0,0 +1,12 @@
+package br.com.moratomicro.maratonajava.javacore.Csobrecargametodos.test;
+
+import br.com.moratomicro.maratonajava.javacore.Csobrecargametodos.dominio.Anime;
+
+public class AnimeTest01 {
+    public static void main(String[] args) {
+        Anime anime = new Anime();
+        anime.init("Akudama Drive", "TV", 12, "Ação");
+
+        anime.imprime();
+    }
+}
