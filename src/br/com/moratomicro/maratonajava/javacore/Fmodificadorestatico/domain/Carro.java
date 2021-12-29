30cc0723db909737f1134ef3746eb77b5568898c
moratomicro <moratomicro@yahoo.com.br>
2021-12-29 08:40:28 -0300

    Orientação a Objetos - Métodos estáticos

--- src/br/com/moratomicro/maratonajava/javacore/Fmodificadorestatico/domain/Carro.java
+++ src/br/com/moratomicro/maratonajava/javacore/Fmodificadorestatico/domain/Carro.java
@@ -3,18 +3,26 @@ package br.com.moratomicro.maratonajava.javacore.Fmodificadorestatico.domain;
 public class Carro {
     private String nome;
     private double velocidadeMaxima;
-    public static double velocidadeLimite = 250;
+    private static double velocidadeLimite = 250;
 
     public Carro(String nome, double velocidadeMaxima) {
         this.nome = nome;
         this.velocidadeMaxima = velocidadeMaxima;
     }
 
-    public  void imprime(){
+    public void imprime(){
         System.out.println("____________________");
         System.out.println("Nome "+this.nome);
         System.out.println("Velocidade máxima "+ this.velocidadeMaxima);
         System.out.println("Velocidade limite "+ Carro.velocidadeLimite);
+    }
+
+    public static void setVelocidadeLimite(double velocidadeLimite) {
+        Carro.velocidadeLimite = velocidadeLimite;
+    }
+
+    public static double getVelocidadeLimite(){
+        return Carro.velocidadeLimite;
     }
 
     public String getNome() {
--- src/br/com/moratomicro/maratonajava/javacore/Fmodificadorestatico/test/CarroTest01.java
+++ src/br/com/moratomicro/maratonajava/javacore/Fmodificadorestatico/test/CarroTest01.java
@@ -4,11 +4,12 @@ import br.com.moratomicro.maratonajava.javacore.Fmodificadorestatico.domain.Carr
 
 public class CarroTest01 {
     public static void main(String[] args) {
+        Carro.setVelocidadeLimite(180);
+
         Carro c1 = new Carro("BMW", 280);
         Carro c2 = new Carro("Mercedes", 275);
         Carro c3 = new Carro("Audi", 290);
 
-        Carro.velocidadeLimite = 180;
 
         c1.imprime();
         c2.imprime();
